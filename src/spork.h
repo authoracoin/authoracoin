375fcf309f31ef5800856c67b0665bdf504a4ce9
authoracoin <authoracoin1@gmail.com>
2019-01-09 08:52:14 +0300

    Update spork.h

--- src/spork.h
+++ src/spork.h
@@ -34,7 +34,6 @@ using namespace boost;
 #define SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT 10007
 #define SPORK_9_MASTERNODE_BUDGET_ENFORCEMENT 10008
 #define SPORK_10_MASTERNODE_PAY_UPDATED_NODES 10009
-#define SPORK_11_RESET_BUDGET 10010
 #define SPORK_12_RECONSIDER_BLOCKS 10011
 #define SPORK_13_ENABLE_SUPERBLOCKS 10012
 #define SPORK_14_NEW_PROTOCOL_ENFORCEMENT 10013
@@ -47,7 +46,6 @@ using namespace boost;
 #define SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT_DEFAULT 4070908800 //OFF
 #define SPORK_9_MASTERNODE_BUDGET_ENFORCEMENT_DEFAULT 4070908800  //OFF
 #define SPORK_10_MASTERNODE_PAY_UPDATED_NODES_DEFAULT 4070908800  //OFF
-#define SPORK_11_RESET_BUDGET_DEFAULT 0
 #define SPORK_12_RECONSIDER_BLOCKS_DEFAULT 0
 #define SPORK_13_ENABLE_SUPERBLOCKS_DEFAULT 4070908800            //OFF
 #define SPORK_14_NEW_PROTOCOL_ENFORCEMENT_DEFAULT 4070908800      //OFF
@@ -114,7 +112,7 @@ public:
     int GetSporkIDByName(std::string strName);
     bool UpdateSpork(int nSporkID, int64_t nValue);
     bool SetPrivKey(std::string strPrivKey);
-    bool CheckSignature(CSporkMessage& spork);
+    bool CheckSignature(CSporkMessage& spork, bool fCheckSigner = false);
     bool Sign(CSporkMessage& spork);
     void Relay(CSporkMessage& msg);
 };
