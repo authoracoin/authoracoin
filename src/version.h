af6d6ad76df4edc8c344cc3eb6e991400a6a0d63
authoracoin <authoracoin1@gmail.com>
2019-01-10 12:00:18 +0300

    Update version.h

--- src/version.h
+++ src/version.h
@@ -13,17 +13,17 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70917;
+static const int PROTOCOL_VERSION = 70918;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 70077;
+// static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70916;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70917;
+static const int MIN_PEER_PROTO_VERSION = 70917;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70918;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -39,6 +39,6 @@ static const int MEMPOOL_GD_VERSION = 60002;
 static const int NO_BLOOM_VERSION = 70005;
 
 //! Community proposal starts with this version
-static const int COMMUNITY_PROPOSAL_VERSION = 70916;
+// static const int COMMUNITY_PROPOSAL_VERSION = 70916;
 
 #endif // BITCOIN_VERSION_H
